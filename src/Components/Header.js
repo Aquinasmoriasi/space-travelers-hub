b765d14bd65b9028eabcbfe540b164af022035ce
unknown <aquinasmoriasi@gmail.com>
2022-08-29 17:57:34 +0300

    add styling to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../Assets/planet.png';
 
 const Header = () => (
-    <header>
-        <div className='logo'>
-            <img src='src/Assets/planet.png'></img>
-            <h1>Space Travelers' Hub</h1>
-        </div>
-        <nav>
-            <Link className='rockets' to='/'></Link>
-            <Link className='missions' to='/'></Link>
-            <Link className='profile' to='/'></Link>
-        </nav>
-    </header>
-)
+  <header>
+    <div className="logo">
+      <img src={logo} alt="logo" />
+      <h1>Space Travelers&apos; Hub</h1>
+    </div>
+    <nav>
+      <Link className="rockets" to="/">Rockets</Link>
+      <Link className="missions" to="/">Missions</Link>
+      <Link className="profile" to="/">Profile</Link>
+    </nav>
+  </header>
+);
+
+export default Header;
