e32619bc16366641c3924affec57e0a330dc3856
bengigenc <genc.bengi@gmail.com>
2022-08-30 11:04:34 +0300

    Fix linter issues

--- src/Components/Missions/Missions.js
+++ src/Components/Missions/Missions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Missions = () => (
-  <div></div>
+  <div />
 );
 
 export default Missions;
--- src/Components/Rockets/Rockets.js
+++ src/Components/Rockets/Rockets.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Rockets = () => (
-  <div></div>
+  <div />
 );
 
 export default Rockets;
