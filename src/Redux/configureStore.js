f4c56ed4727fda43f0f538c29abf37535e908ba1
bengigenc <genc.bengi@gmail.com>
2022-08-30 19:23:45 +0300

    Fetch missions data from the api

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import rocketsReducer from './rockets/rockets';
 
-const rootReducer = combineReducers({ 
+const rootReducer = combineReducers({
   rockets: rocketsReducer,
   // add missions reducer here
 });
