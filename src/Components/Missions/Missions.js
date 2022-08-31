e3410149a74ffa71fca1d6796c090d8415900ad6
bengigenc <genc.bengi@gmail.com>
2022-08-31 16:59:19 +0300

    Style and position mission list layout

--- src/Components/Missions/Missions.js
+++ src/Components/Missions/Missions.js
@@ -18,31 +18,33 @@ const Missions = () => {
   const missionHeadings = ['Missions', 'Description', 'Status', 'Action'];
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          {missionHeadings.map((heading) => (
-            <th key={heading}>{heading}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {missions.map((mission) => (
-          <tr key={mission.mission_id}>
-            <td>{mission.mission_name}</td>
-            <td>{mission.description}</td>
-            <td>
-              <Badge bg="secondary">NOT A MEMBER</Badge>
-            </td>
-            <td>
-              <Button variant="outline-secondary" type="button">
-                Join Mission
-              </Button>
-            </td>
+    <section className="table-section pb-4 pt-2 ms-4 me-4 border-top rounded-0 border-2">
+      <Table className="mt-2" striped bordered hover>
+        <thead>
+          <tr>
+            {missionHeadings.map((heading) => (
+              <th key={heading}>{heading}</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td className="text-rigth">{mission.mission_name}</td>
+              <td className="description-col">{mission.description}</td>
+              <td>
+                <Badge bg="secondary">NOT A MEMBER</Badge>
+              </td>
+              <td>
+                <Button variant="outline-secondary" type="button">
+                  Join Mission
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </section>
   );
 };
 
