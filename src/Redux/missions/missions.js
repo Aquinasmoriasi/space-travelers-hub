db285848cae6011e8d586761a112bed29f54305a
bengigenc <genc.bengi@gmail.com>
2022-08-31 18:06:36 +0300

    Update reducer for joining missions

--- src/Redux/missions/missions.js
+++ src/Redux/missions/missions.js
@@ -1,17 +1,25 @@
 const missionsUrl = 'https://api.spacexdata.com/v3/missions';
 
-const SHOW_MISSIONS = 'space-travelers-hub/missions/missions';
+const SHOW_MISSIONS = 'space-travelers-hub/missions/SHOW_MISSIONS';
+
+const JOIN_MISSIONS = 'space-travelers-hub/missions/JOIN_MISSIONS';
 
 export const showMissions = (data) => ({
   type: SHOW_MISSIONS,
   data,
 });
 
+export const joinMission = (id) => ({
+  type: JOIN_MISSIONS,
+  id,
+});
+
 const filteredMissions = (data) => {
   const missions = data.map((mission) => ({
-    mission_id: mission.mission_id,
+    id: mission.mission_id,
     mission_name: mission.mission_name,
     description: mission.description,
+    reserved: false,
   }));
   return showMissions(missions);
 };
@@ -26,7 +34,11 @@ export default function missionsReducer(state = [], action) {
   switch (action.type) {
     case SHOW_MISSIONS:
       return action.data;
+    case JOIN_MISSIONS:
+      return state.map((mission) => {
+        if (mission.id !== action.id) return mission;
+        return { ...mission, reserved: true };
+      });
     default:
       return state;
-  }
-}
+  }}
