48d27b03293e6c2528f53d67e824be23daf94c39
bengigenc <genc.bengi@gmail.com>
2022-09-01 17:49:23 +0300

    Fix linter issues

--- src/tests/missions.test.js
+++ src/tests/missions.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../Redux/configureStore';
-import Missions from '../components/Missions/Missions.js';
+import Missions from '../components/Missions/Missions';
 import missionsReducer, {
   showMissions,
   joinMission,
