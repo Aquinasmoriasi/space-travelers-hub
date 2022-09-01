3e992e38f73c5ea58727c5cb10937fd28ea2391b
bengigenc <genc.bengi@gmail.com>
2022-09-01 13:15:00 +0300

    Display active missions on the profile page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Table } from 'react-bootstrap';
 
-const Profile = () => (
-  <div />
-);
+const Profile = () => {
+  const missions = useSelector((state) => state.missions);
+
+  const activeMissions = missions.filter((mission) => mission.reserved);
+
+  return (
+    <section className="profile-tables">
+      <div className="mission-list">
+        <h2>My Missons</h2>
+        <Table bordered>
+          <tbody>
+            {activeMissions.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </section>
+  );
+};
 
 export default Profile;
